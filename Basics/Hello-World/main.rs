ca998ca00be067c52fe23856672ea7419bc4f7af
kbpatel <kbpatel3@catamount.wcu.edu>
2023-12-16 13:34:29 -0500

    First ever Rust program. Hello World!

---
+++ Basics/Hello-World/main.rs
@@ -0,0 +1,16 @@
+/**
+ Hello-World
+ @file main.rs
+ @author Kaushal Patel
+*/
+
+/**
+ * The main function is the entry point of the program.
+ * Will print "Hello, World!" to the console.
+ * @fn main
+ * @return void
+ */
+fn main() {
+    // Use the println! macro to print to the console.
+    println!("Hello, World!");
+}
